670d24a9855d9bd13669e77019506f657273ef39
Benito Sanchez <sanchezbenitog@gmail.com>
2016-11-26 12:16:50 -0800

    Adding help Alert screen and making it scrollable
    Summary:
    Added a alert dialog on help button clicked
    Made alert dialog scrollable when content is long
    
    Test Plan:
    Patch to colore
    click on Help button
    verify that an alert dialog is displayed
    verify that when scrolled the content scrolls down
    
    Reviewers: AnitaGarcia819
    
    Reviewed By: AnitaGarcia819
    
    Differential Revision: https://reviews.facebook.net/D65889

--- colore/HomeViewController.swift
+++ colore/HomeViewController.swift
@@ -12,4 +12,21 @@ class HomeViewController: UIViewController{
     override func viewDidLoad() {
         super.viewDidLoad()
     }
+    
+    /* 
+     * When Help button is clicked, shows the alert dialog.
+    */
+    @IBAction func onHelpButtonClicked(sender: UIButton) {
+        
+        let alert = UIAlertController(
+                    title: "Instructions",
+                    message: "long string\nlong string\nlong string\nlong string\nlong string\nlong string\nlong string\nlong string\nlong string\nlong string\nlong string\nlong string\nlong string\nlong string\nlong string\nlong string\nlong string\nlong string\nlong string\nlong string\nlong string\nlong string\nlong string\nlong string\nlong string\nlong string\nlong string\nlong string\nlong string\nlong string\nlong string\nlong string\nlong string\nlong string\nlong string\nlong string\nlong string\nlong string\nlong string\nlong string\nlong string\nlong string\n",
+                    preferredStyle: UIAlertControllerStyle.Alert)
+        
+        let OKAction = UIAlertAction(title: "OK", style: .Default) { (action) in }
+        
+        alert.addAction(OKAction)
+        
+        presentViewController(alert, animated: true, completion: nil)
+    }
 }
